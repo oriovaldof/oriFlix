b1d502a32429e62dffc11861ced82af7180698d5
Oriovaldo de Jesus Fialho <oriovaldof@gmail.com>
2020-07-29 14:28:22 -0400

    categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,4 +1,4 @@
-categoriaimport React, { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
