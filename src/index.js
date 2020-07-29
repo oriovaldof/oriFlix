182dad303f4273b090aebeea5d321cf10e78ce28
Oriovaldo de Jesus Fialho <oriovaldof@gmail.com>
2020-07-29 09:56:28 -0400

    ajuste pages e categoria

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import Page404 from './pages/Page404';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Page404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
---
+++ src/pages/Page404/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageDefault from '../../components/PageDefault';
+
+function Page404() {
+    return (
+        <PageDefault>
+            <h1>Ooops essa Pagina não Existe!!</h1>
+        </PageDefault>
+    );
+}
+
+export default Page404;
